aa2de81ff065ada0387e9e5b406a052015ceff68
augustineezinwa <augustineezinwa@gmail.com>
2018-08-09 11:19:25 +0100

    fix gh-pages

--- UI/js/toggle.js
+++ UI/js/toggle.js
@@ -2,15 +2,11 @@ function toggle () {
     
     const nav = document.querySelectorAll('.callNav');
    
-    nav.forEach(x => {
-        if(x.style.display === '' || x.style.display === 'none') {
-            x.style.display = 'block';
-        } else {
-            x.style.display = '';
-        }
-    });
+     nav.forEach(x => { 
+         x.classList.toggle('invisible');
+         x.classList.toggle('render');
+        }); 
    console.log(nav);
 }
 
-const dropdownButton = document.querySelector('.dropdown-icon');
-dropdownButton.addEventListener('click', toggle);
+ 
