a406faae2fbd9c9bfc52ae5e92d93697cebc53cb
augustineezinwa <augustineezinwa@gmail.com>
2018-08-09 15:27:13 +0100

    fix gh-pages

--- UI/js/toggle.js
+++ UI/js/toggle.js
@@ -1,12 +1,6 @@
-function toggle () {
-    
-    const nav = document.querySelectorAll('.callNav');
-   
-     nav.forEach(x => { 
-         x.classList.toggle('invisible');
-         x.classList.toggle('render');
-        }); 
-   console.log(nav);
-}
-
- 
+function toggle() {
+    const nav = document.querySelector('.getNav');
+    console.log(nav);
+    return nav.classList.toggle('nav-bar-toggle-display');
+  }
+  document.querySelector('.nav-bar-icon-toggle').addEventListener('click', toggle);
