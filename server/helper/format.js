12fb15d4dca50e86276b7c7723ddb00a3cfe5647
augustineezinwa <augustineezinwa@gmail.com>
2018-09-01 04:05:42 +0100

    bg(fix-get-question-response): eliminate null from numberofanswers
    - refactor codebase
    - remove number of answers field
    
    [Finishes #160213606]

--- server/helper/format.js
+++ server/helper/format.js
@@ -1,3 +1,18 @@
+const formatQuestionsWithAnswers = (data) => {
+  const newQuestions = [];
+  data.forEach((x) => {
+    newQuestions.push({
+      id: x.id,
+      questionTitle: x.questiontitle,
+      questionDescription: x.questiondescription,
+      answers: [],
+      time: x.time,
+      date: x.date,
+      userId: x.userid
+    });
+  });
+  return newQuestions;
+};
 
 const formatQuestions = (data) => {
   const newQuestions = [];
@@ -71,5 +86,5 @@ const formatComments = (data) => {
 };
 
 export {
-  formatQuestions, formatAnswers, formatComments, formatMostAnsweredQuestions
+  formatQuestions, formatAnswers, formatComments, formatMostAnsweredQuestions, formatQuestionsWithAnswers
 };
