ccd427a3cd70ea8c116db8903e9b02e53a871960
augustineezinwa <augustineezinwa@gmail.com>
2018-09-11 11:52:36 +0100

    ft(render-all-questions-clientside): render questions on homepage
    - add views for questions
    - add view contollers for questions
    - add api controllers for questions
    - add load more pagination feature
    
    [Finishes #160415283]

--- client/public/js/src/views/home.js
+++ client/public/js/src/views/home.js
@@ -12,253 +12,34 @@ const home = ` <div class = "container image-background" style = "margin: 0 auto
 
 <div class = "container dashboardfooter" ><h3>Trending Questions</h3></div>
 
-
-<div class = "container " style = "background-color: #f1f1f1">
-<div class = "row">
-
-<div class = "col">
-
-  <div class = "card">
-    <div class = "container">
-      <div class = "row mt-4 pd-1" >
-        <div class = "col-2">
-          <div class = "symbol-display">
-            <div class = "alignSymbol">A</div>
-          </div>
-          
-        </div>
-        <div class = "col-5">
-          <div class = "question">How do I recusrsively return a div from my function?</div>
-
-        </div>
-      </div>
-      <div class = "row mt-2 pd-1">
-        <div class = "col"> <span> 10 Answers</span></div>
-        <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-        <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-        <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-      </div>
-    </div>
-  </div>
-
-</div>
-<div class = "col">
-
-    <div class = "card">
-        <div class = "container">
-          <div class = "row mt-4 pd-1" >
-            <div class = "col-2">
-              <div class = "symbol-display">
-                <div class = "alignSymbol">A</div>
-              </div>
-              
-            </div>
-            <div class = "col-5">
-              <div class = "question">How do I recusrsively return a div from my function?</div>
-
-            </div>
-          </div>
-          <div class = "row mt-2 pd-1">
-            <div class = "col"> <span> 10 Answers</span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-            <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-          </div>
-        </div>
-      </div>
-
-</div>
-<div class = "col">
-
-    <div class = "card">
-        <div class = "container">
-          <div class = "row mt-4 pd-1" >
-            <div class = "col-2">
-              <div class = "symbol-display">
-                <div class = "alignSymbol">A</div>
-              </div>
-              
-            </div>
-            <div class = "col-5">
-              <div class = "question">How do I recusrsively return a div from my function?</div>
-
-            </div>
-          </div>
-          <div class = "row mt-2 pd-1">
-            <div class = "col"> <span> 10 Answers</span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-            <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-          </div>
-        </div>
-      </div>
-
-</div>
-</div>
-
-<div class = "row">
-
-  <div class = "col">
-
-    <div class = "card">
-      <div class = "container">
-        <div class = "row mt-4 pd-1" >
-          <div class = "col-2">
-            <div class = "symbol-display">
-              <div class = "alignSymbol">A</div>
-            </div>
-            
-          </div>
-          <div class = "col-5">
-            <div class = "question">How do I recusrsively return a div from my function?</div>
-
-          </div>
-        </div>
-        <div class = "row mt-2 pd-1">
-          <div class = "col"> <span> 10 Answers</span></div>
-          <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-          <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-          <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-  <div class = "col">
-
-      <div class = "card">
+<div id= "questionsDisplay">
+<div class = "container" >
+<div class ="row no-questions" >
+  
+  <div class ="alignCardWidth" >
+      <div class = "card" >
           <div class = "container">
             <div class = "row mt-4 pd-1" >
               <div class = "col-2">
                 <div class = "symbol-display">
-                  <div class = "alignSymbol">A</div>
+                  <div class = "alignSymbol" >!</div>
                 </div>
                 
               </div>
               <div class = "col-5">
-                <div class = "question">How do I recusrsively return a div from my function?</div>
+                <div class = "question" >No Questions yet!  &nbsp Refresh page  </div>
 
               </div>
             </div>
-            <div class = "row mt-2 pd-1">
-              <div class = "col"> <span> 10 Answers</span></div>
-              <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-              <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-              <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-            </div>
+           
+              <div class = "col" style="text-align:right"><span></span><span></span><a href ="/"><button type= "answer">Refresh</button></a></div>
+           
           </div>
         </div>
-
   </div>
-  <div class = "col">
-
-      <div class = "card">
-          <div class = "container">
-            <div class = "row mt-4 pd-1" >
-              <div class = "col-2">
-                <div class = "symbol-display">
-                  <div class = "alignSymbol">A</div>
-                </div>
-                
-              </div>
-              <div class = "col-5">
-                <div class = "question">How do I recusrsively return a div from my function?</div>
-
-              </div>
-            </div>
-            <div class = "row mt-2 pd-1">
-              <div class = "col"> <span> 10 Answers</span></div>
-              <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-              <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-              <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-            </div>
-          </div>
-        </div>
-
-  </div>
+  
 </div>
-
-<div class = "row">
-
-    <div class = "col">
-
-      <div class = "card">
-        <div class = "container">
-          <div class = "row mt-4 pd-1" >
-            <div class = "col-2">
-              <div class = "symbol-display">
-                <div class = "alignSymbol">A</div>
-              </div>
-              
-            </div>
-            <div class = "col-5">
-              <div class = "question">How do I recusrsively return a div from my function?</div>
-
-            </div>
-          </div>
-          <div class = "row mt-2 pd-1">
-            <div class = "col"> <span> 10 Answers</span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-            <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-            <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    <div class = "col">
-
-        <div class = "card">
-            <div class = "container">
-              <div class = "row mt-4 pd-1" >
-                <div class = "col-2">
-                  <div class = "symbol-display">
-                    <div class = "alignSymbol">A</div>
-                  </div>
-                  
-                </div>
-                <div class = "col-5">
-                  <div class = "question">How do I recusrsively return a div from my function?</div>
-
-                </div>
-              </div>
-              <div class = "row mt-2 pd-1">
-                <div class = "col"> <span> 10 Answers</span></div>
-                <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-                <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-                <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-              </div>
-            </div>
-          </div>
-
-    </div>
-    <div class = "col">
-
-        <div class = "card">
-            <div class = "container">
-              <div class = "row mt-4 pd-1" >
-                <div class = "col-2">
-                  <div class = "symbol-display">
-                    <div class = "alignSymbol">A</div>
-                  </div>
-                  
-                </div>
-                <div class = "col-5">
-                  <div class = "question">How do I recusrsively return a div from my function?</div>
-
-                </div>
-              </div>
-              <div class = "row mt-2 pd-1">
-                <div class = "col"> <span> 10 Answers</span></div>
-                <div class = "col"><span> <i class="fas fa-thumbs-up"></i></span>&nbsp 200<span></span></div>
-                <div class = "col"><span> <i class="fas fa-thumbs-down "></i></span>&nbsp 200<span><span></span></div>
-                <div class = "col"><span></span><span></span><a href ="questions.html"><button type= "answer">View</button></a></div>
-              </div>
-            </div>
-          </div>
-
-    </div>
-  </div>
+</div>
 
 </div>
 `;
