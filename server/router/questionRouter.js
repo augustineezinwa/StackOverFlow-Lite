210bc8d2e5a352fa158fb47b4f2d615722c3d052
Augustine Ezinwa <augustineezinwa@gmail.com>
2026-02-12 02:15:47 +0100

    fix deploy

--- server/router/questionRouter.js
+++ server/router/questionRouter.js
@@ -4,26 +4,40 @@ import QuestionValidation from '../middlewares/QuestionValidation.js';
 import Security from '../middlewares/Security.js';
 import AnswerController from '../controllers/AnswerController.js';
 
-const {
-  fetchQuestions, fetchAQuestion, addQuestion, deleteQuestion, fetchUserQuestions,
-  fetchSearchedQuestions, fetchQuestionsWithMostAnswers
-} = QuestionController;
-const { fetchAnswersForAQuestion } = AnswerController;
-const {
-  validateQuestionTitle, validateQuestionDescription, validatePermissionToDeleteQuestion,
-  validateQuestionExistence, validateUrl
-} = QuestionValidation;
-const { guardRoute } = Security;
+const QuestionControllerClass = QuestionController.default || QuestionController;
+const AnswerControllerClass = AnswerController.default || AnswerController;
+const QuestionValidationClass = QuestionValidation.default || QuestionValidation;
+const SecurityClass = Security.default || Security;
 
 const questionRouter = express.Router();
 
-questionRouter.get('/questions', fetchSearchedQuestions, fetchQuestions);
-questionRouter.get('/questions/mostanswers', fetchQuestionsWithMostAnswers);
-questionRouter.get('/users/questions', guardRoute, fetchUserQuestions);
-questionRouter.get('/questions/:questionId', validateUrl, fetchAnswersForAQuestion, fetchAQuestion);
-questionRouter.post('/questions', validateQuestionTitle, validateQuestionDescription,
-  guardRoute, addQuestion);
-questionRouter.delete('/questions/:questionId', validateUrl, guardRoute, validateQuestionExistence,
-  validatePermissionToDeleteQuestion, deleteQuestion);
+questionRouter.get(
+  '/questions',
+  QuestionControllerClass.fetchSearchedQuestions,
+  QuestionControllerClass.fetchQuestions
+);
+questionRouter.get('/questions/mostanswers', QuestionControllerClass.fetchQuestionsWithMostAnswers);
+questionRouter.get('/users/questions', SecurityClass.guardRoute, QuestionControllerClass.fetchUserQuestions);
+questionRouter.get(
+  '/questions/:questionId',
+  QuestionValidationClass.validateUrl,
+  AnswerControllerClass.fetchAnswersForAQuestion,
+  QuestionControllerClass.fetchAQuestion
+);
+questionRouter.post(
+  '/questions',
+  QuestionValidationClass.validateQuestionTitle,
+  QuestionValidationClass.validateQuestionDescription,
+  SecurityClass.guardRoute,
+  QuestionControllerClass.addQuestion
+);
+questionRouter.delete(
+  '/questions/:questionId',
+  QuestionValidationClass.validateUrl,
+  SecurityClass.guardRoute,
+  QuestionValidationClass.validateQuestionExistence,
+  QuestionValidationClass.validatePermissionToDeleteQuestion,
+  QuestionControllerClass.deleteQuestion
+);
 
 export default questionRouter;
