fd567474c935e1a4650e73f42657b79c35de10fb
Augustine Ezinwa <augustineezinwa@gmail.com>
2026-02-12 02:30:48 +0100

    fix build

--- server/router/baseRouter.js
+++ server/router/baseRouter.js
@@ -5,7 +5,26 @@ import userRouter from './userRouter.js';
 import voteRouter from './voteRouter.js';
 import defaultRouter from './defaultRouter.js';
 
+const unwrapModule = (moduleRef) => {
+  let resolved = moduleRef;
+  while (resolved && resolved.default) {
+    resolved = resolved.default;
+  }
+  return resolved || moduleRef;
+};
+
+const toRouter = (resolvedModule) => {
+  if (typeof resolvedModule === 'function') return resolvedModule;
+  return (request, response, next) => next(new Error('Missing router middleware'));
+};
+
+const questionRoutes = toRouter(unwrapModule(questionRouter));
+const answerRoutes = toRouter(unwrapModule(answerRouter));
+const userRoutes = toRouter(unwrapModule(userRouter));
+const voteRoutes = toRouter(unwrapModule(voteRouter));
+const defaultRoutes = toRouter(unwrapModule(defaultRouter));
+
 const baseRouter = express.Router();
-baseRouter.use(questionRouter, answerRouter, userRouter, voteRouter, defaultRouter);
+baseRouter.use(questionRoutes, answerRoutes, userRoutes, voteRoutes, defaultRoutes);
 
 export default baseRouter;
